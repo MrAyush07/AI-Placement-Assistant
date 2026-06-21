f4849b2df749cd418eece69352dfcb99b365543f
Ayush Yadav <yayush199@gmail.com>
2026-06-21 15:05:26 +0530

    Production update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-placement-assistant-ff16.onrender.com",
+  baseURL:
+    "https://ai-placement-assistant-ff16.onrender.com/api",
 });
 
 export default api;
